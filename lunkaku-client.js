ce63b25cb334a054270cb71582258f9cf60244d2
Masato Hori <rillomas@gmail.com>
2011-07-02 16:48:03 +0900

    added default value for geolocation

--- lunkaku-client.js
+++ lunkaku-client.js
@@ -1,30 +1,32 @@
 function getNearbyLunchRemarks() {
     console.log("getting nearby restaurants")
+    var coordinate =  { lon: 139.761146, lat : 35.670842 };
     navigator.geolocation.getCurrentPosition(
         function(pos){
-            $.getJSON(
-                "http://lunkaku.appspot.com/list",
-                { lon: pos.coords.longitude, lat : pos.coords.latitude },
-                function(data, status){
-                    var items = [];
-                    $.each(data, function(key, val) {
-                        items.push(
-                            '<li>' +
-                                '<a href="' + "url/comes/here" + '">' +
-                                    '<img src="' + val.image + '" />' +
-                                    '<p>' + val.content + '</p>' +
-                                '</a>' +
-                            '</li>'
-                        );
-                    });
-                    var joined = items.join('');
-                    $('#result').html(joined)
-                    console.log("created list")
-                });
+            coordinate.lon = pos.coords.longitude;
+            coodrinate.lat = pos.coords.latitude;
         },
         function(error){
-            alert("Your geolocation was not able to be retrieved!")
-        },
-        {enableHighAccuracy:true,timeout:1000,maximumAge:0}
+            console.log("Your geolocation was not able to be retrieved!")
+        }
     );
+
+    $.getJSON(
+        "http://lunkaku.appspot.com/list",
+        coordinate,
+        function(data, status){
+            var items = [];
+            $.each(data, function(key, val) {
+                       items.push(
+                           '<li>' +
+                               '<a href="' + "url/comes/here" + '">' +
+                               '<img src="' + val.image + '" />' +
+                               '<p>' + val.content + '</p>' +
+                               '</a>' +
+                           '</li>'
+                       );
+            });
+            var joined = items.join('');
+            $('#result').html(joined);
+    });
 }
