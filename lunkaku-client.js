dde04e17a3161f92b6430e05164237c36f2b45d8
Masato Hori <rillomas@gmail.com>
2011-07-02 15:49:41 +0900

    added geolocation API

--- lunkaku-client.js
+++ lunkaku-client.js
@@ -1,26 +1,30 @@
 function getNearbyLunchRemarks() {
     console.log("getting nearby restaurants")
-    $.getJSON(
-        "http://lunkaku.appspot.com/list",
-        { lon: 11.1, lat : 12.3 },
-        function(data, status){
-            var items = [];
-
-            $.each(data, function(key, val) {
-                  items.push(
-                      '<li>' +
-                          '<a href="' + "url/comes/here" + '">' +
-                              '<img src="' + val.image + '" />' +
-                              '<p>' + val.content + '</p>' +
-                           '</a>' +
-                       '</li>'
-                  );
-             });
-
-            var joined = items.join('');
-            alert(joined);
-            $('#result').html(joined)
-            console.log("created list")
-        }
+    navigator.geolocation.getCurrentPosition(
+        function(pos){
+            $.getJSON(
+                "http://lunkaku.appspot.com/list",
+                { lon: pos.coords.longitude, lat : pos.coords.latitude },
+                function(data, status){
+                    var items = [];
+                    $.each(data, function(key, val) {
+                        items.push(
+                            '<li>' +
+                                '<a href="' + "url/comes/here" + '">' +
+                                    '<img src="' + val.image + '" />' +
+                                    '<p>' + val.content + '</p>' +
+                                '</a>' +
+                            '</li>'
+                        );
+                    });
+                    var joined = items.join('');
+                    $('#result').html(joined)
+                    console.log("created list")
+                });
+        },
+        function(error){
+            alert("Your geolocation was not able to be retrieved!")
+        },
+        {enableHighAccuracy:true,timeout:1000,maximumAge:0}
     );
 }
